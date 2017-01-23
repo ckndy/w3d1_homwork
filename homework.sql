633ef4ceedafc19618c904522779b74f48623c8e
ckndy <chris.kennedy@live.co.uk>
2017-01-23 18:46:24 +0000

    added homework

---
+++ homework.sql
@@ -0,0 +1,22 @@
+SELECT * FROM movies;
+
+SELECT name FROM people;
+
+UPDATE people SET name = 'Alastair Hood' WHERE name = 'Allesdor Hood';
+
+SELECT name FROM people WHERE name = 'Chris Kennedy';
+
+DELETE FROM movies WHERE title = 'Batman Begins';
+
+INSERT INTO people name VALUES 'John McCollum'
+
+DELETE FROM people WHERE name 'Harvey Wheaton';
+
+INSERT INTO movies title AND show_time VALUES ('Guardians of the Galaxy Vol. 2'; 2017, '00:00')
+
+UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
+
+
+#EXTENSION
+
+DELETE FROM movies WHERE title = 'Captain America: The First Avenger' AND title = 'Captain America: The Winter Soldier' AND title = 'Captain America: Civil War';
